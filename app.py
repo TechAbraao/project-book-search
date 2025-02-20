774042a0602a5dd4a0cb1d98058d32e2ef3dc72c
Abraão Santos <abraao.santos@sec4you.com.br>
2025-02-20 00:35:26 -0300

    algumas mudanças

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
+@app.route("/")
+def root():
+    return redirect(url_for('home'))
 
 @app.route("/home")
 def home():
