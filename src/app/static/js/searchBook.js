82eb3a3a9973c508f015fe633fb9452ed17a533a
Abraão Santos <abraao.santos@sec4you.com.br>
2025-04-02 19:58:26 -0300

    feat: finish v1.0.0

---
+++ src/app/static/js/searchBook.js
@@ -0,0 +1,30 @@
+document.getElementById("input").addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+
+        let valor = this.value;
+
+        fetch('{{ url_for("home.index") }}', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ message: valor })
+        })
+            .then(response => {
+                if (response.redirected) {
+                    window.location.href = response.url;
+                } else {
+                    return response.text();
+                }
+            })
+            .then(html => {
+                document.open();
+                document.write(html);
+                document.close();
+            })
+            .catch(error => console.error('Erro:', error));
+
+        this.value = "";
+    }
+});
